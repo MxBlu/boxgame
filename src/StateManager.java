6a3c3487e6775b2a6780a9e5c324147fe0bb5bc4
Jumail <mundekkat@hotmail.com>
2017-05-11 19:52:22 +1000

    This one runs

--- src/StateManager.java
+++ src/StateManager.java
@@ -16,10 +16,8 @@ public class StateManager {
 	
 	public static void setState(String stateName) {
 		if (stateName.equals("INTRO")) {
-			currentState = null;
 			currentState = new BootState();
 		} else if (stateName.equals("INTRO2")) {
-			currentState = null;
 			currentState = new Boot2State(); 
 		} else if (stateName.equals("MENU")) {
 		   currentState = new MenuState();
