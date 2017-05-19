716ff862de35a61d05b5146235d113d69514ef5e
DengEm <deng.emily@gmail.com>
2017-05-19 17:04:48 +1000

    trial

---
+++ src/MenuStateTrial.java
@@ -0,0 +1,73 @@
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+public class MenuStateTrial implements GameState{
+
+	private JButton play;
+	private JButton credits;
+	private JButton highScore;
+	private JPanel gamePanel;
+	public MenuStateTrial() {
+		init();
+		
+		
+	}
+
+	@Override
+	public void init() {
+		play = new JButton("Play");
+		credits = new JButton("Credits");
+		highScore = new JButton("High Score");
+		gamePanel = new JPanel(new FlowLayout());
+		gamePanel.add(play);
+		gamePanel.add(credits);
+		gamePanel.add(highScore);
+		gamePanel.revalidate();
+		gamePanel.setBackground(Color.BLUE);
+		StateManager.getFrame().add(gamePanel);
+		System.out.println("finished in menu");
+		
+	}
+
+	@Override
+	public void update() {
+		handleInput();
+		
+		
+	}
+
+	@Override
+	public void draw(Graphics2D g) {
+		Font font = new Font("arial", Font.BOLD, 50);
+		g.setFont(font);
+		g.setColor(Color.WHITE);
+		g.drawString("BLOCK STUFF",GameMaster.WIDTH/2 , 100);
+		System.out.println("inside the block for draw mentustate ");
+	}
+
+	@Override
+	public void handleInput() {
+		play.addActionListener(new ActionListener(){
+			public void actionPerformed (ActionEvent e){
+				StateManager.getFrame().removeAll();
+				StateManager.setState("LEVEL");		
+			}
+		});
+		
+		credits.addActionListener(new ActionListener(){
+			public void actionPerformed (ActionEvent e){
+				StateManager.getFrame().removeAll();
+				StateManager.setState("CREDITS");	
+			}
+		});
+		
+	}
+
+}
