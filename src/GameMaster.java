0b60881748fa894275c9d6d75ace7be653f3e690
Brandon Wong <brandonwong@Brandons-MacBook-Pro.local>
2017-05-20 17:27:38 +1000

    Fixed menu screen

--- src/GameMaster.java
+++ src/GameMaster.java
@@ -2,57 +2,69 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
-import java.awt.CardLayout;
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.awt.image.BufferedImage; 
-
 public class GameMaster{
-	public static final int WIDTH = 1280;
-	public static final int HEIGHT = 720;
-	public static final int FPS = 60;
-	public static final int FRAME_DELTA = 1000/FPS;
+    
+    // The window and game dimensions
+    public static final int WIDTH = 1280;
+    public static final int HEIGHT = 720;
+    
+    // Set the frames per second
+    public static final int FPS = 60;
+    public static final int FRAME_DELTA = 1000/FPS;
 
-	// Here for debugging reasons only
-	public static float adj1Chance = 0.40f;
-	public static float adj2Chance = 0.20f;
-	public static float adj3Chance = 0.50f;
-	public static float adj4Chance = 0.60f;
-	
-	private static JFrame frame;
-	private static JPanel currentPanel;
-	
+    // Here for debugging reasons only
+    public static float adj1Chance = 0.40f;
+    public static float adj2Chance = 0.20f;
+    public static float adj3Chance = 0.50f;
+    public static float adj4Chance = 0.60f;
 
+    // JFrame which holds all of the game's
+    // JPanels and their contents. 
+    private static JFrame frame;
+    
     public static void main(String[] args) {
-    	SwingUtilities.invokeLater(new Runnable() {
+        SwingUtilities.invokeLater(new Runnable() {
             @Override
-            public void run() {                                           
-            	initScreen();
-            	frame.getContentPane().add(new BootScreen());
-            	
+            public void run() { 
+                // Initialize the JFrames attributes
+                initScreen();
+                // Add a boot up screen/JPanel to this
+                // JFrame
+                frame.getContentPane().add(new BootScreen());
+                
             }
         });  
     }
     
     private static void initScreen(){
-    	frame = new JFrame("Game");
+        
+        // Initialize the settings and attributes
+        // for our JFrame. This class, GameMaster
+        // acts as the JFrame. We initialize its
+        // size, and its visibility. 
+        frame = new JFrame("Game");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(WIDTH, HEIGHT);
         frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
         frame.setVisible(true);
+        frame.setFocusable(true);
         /*panel = new JPanel(new CardLayout());
         frame.add(panel);*/
     }
     
     public static void changeScreens(JPanel newScreen) {
-    	frame.getContentPane().removeAll();
-    	frame.getContentPane().add( newScreen);
-		frame.repaint();
-		frame.revalidate();
-		
+        // This function is used to change between
+        // screens. For our implementation, each
+        // screen is stored as a JPanel. This
+        // function removes all the JPanels in the
+        // main JFrame, adds a new screen/JPanel 
+        // to it, and then repaints and revalidates. 
+        frame.getContentPane().removeAll();
+        frame.getContentPane().add(newScreen);
+        frame.repaint();
+        frame.revalidate();
+        
     }
     
     
@@ -62,4 +74,4 @@ public class GameMaster{
     
     
     
-	
+    
--- src/MenuStateTrial.java
+++ src/MenuStateTrial.java
@@ -1,15 +1,27 @@
 import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Image;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class MenuStateTrial extends JPanel{
+
+    private Image background;
+    private Image playbutton;
+    private Image creditsbutton;
+    private Image highscoresbutton;
 
 	private JButton play;
 	private JButton credits;
@@ -18,10 +30,40 @@ public class MenuStateTrial extends JPanel{
 	
 	public MenuStateTrial() {
 		System.out.println("in menu screen");
-		play = new JButton("Play");
-		credits = new JButton("Credits");
-		highScore = new JButton("High Score");
 		
+		// Get the image file for the background and buttons
+		try {
+			background = ImageIO.read(getClass().getResourceAsStream("menu.png"));
+			playbutton = ImageIO.read(getClass().getResourceAsStream("playbutton.png"));
+			creditsbutton = ImageIO.read(getClass().getResourceAsStream("creditsbutton.png"));
+			highscoresbutton = ImageIO.read(getClass().getResourceAsStream("highscoresbutton.png"));
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}		
+		
+		// Initialize JButtons
+		// Give them names, and dimensions. Also
+		// add tool tips on mouse over to assist the user.
+		play = new JButton();
+		play.setIcon(new ImageIcon(playbutton));
+		play.setToolTipText("Click this to begin the game!");
+		play.setBorderPainted(false);
+		
+		credits = new JButton();
+		credits.setIcon(new ImageIcon(creditsbutton));
+		credits.setToolTipText("Click this to see who made the game!");
+		credits.setBorderPainted(false);
+
+		highScore = new JButton();
+		highScore.setIcon(new ImageIcon(highscoresbutton));
+		highScore.setToolTipText("Click this to everyone's highest scores!");
+		highScore.setBorderPainted(false);
+
+		// Play button action listener
+		// When play button is clicked, call the game master
+		// to change the screen. This swaps out the JPanel
+		// to the JPanel that holds the game. 
 		play.addActionListener(new ActionListener(){
 			public void actionPerformed (ActionEvent e){
 				System.out.println("change thing");
@@ -29,21 +71,55 @@ public class MenuStateTrial extends JPanel{
 			}
 		});
 		
+		// Credits button action listener
+		// When the credits button is clicked, call the
+		// game master to change to credits screen.
 		credits.addActionListener(new ActionListener(){
 			public void actionPerformed (ActionEvent e){
 				System.out.println("change screen");
 			}
 		});
 		
-		add(play);
-		add(credits);
-		add(highScore);
+		// Set the layout of this JPanel
+		// We can use one of the preset layouts from
+		// JPanel's API to arrange the layout and 
+		// positions of where components appear in this
+		// JPanel. This is a better alternative opposed
+		// to manually hard coding pixel perfect coordinates
+		// where each button should appear. 
+		setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+
+		// Add the buttons to the JPanel
+		// gridx and gridy are part of JPanel's layout
+		// API, and lets us organise the location of the buttons.
+		c.gridx = 2;
+		c.gridy = 1;
+		add(play,c);
+		c.gridy = 2;
+		add(credits,c);
+		c.gridy = 3;
+		add(highScore,c);
+		
+		// Set the JPanels attributes and revalidate.
 		revalidate();
-		setBackground(Color.BLUE);
 		this.setFocusable(true);
 	    this.requestFocusInWindow();
 		
 	}
+	
+	public void paintComponent(Graphics g) {
+		// Access the JPanel super class's
+		// function for painting.
+		super.paintComponent(g);
+
+	    // Draw the background for this JPanel
+	    g.setColor(Color.BLACK);
+        g.drawImage(background, 0, 0, null);
+        	     
+	}
+	
+	
 /*
 	@Override
 	public void init() {
