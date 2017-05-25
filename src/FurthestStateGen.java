207caaf1362a06428ff1e5cb1d968f7bb5689ed4
Ines <ines.sarmiento1@gmail.com>
2017-05-26 02:17:06 +1000

    Small fix.

--- src/FurthestStateGen.java
+++ src/FurthestStateGen.java
@@ -192,7 +192,7 @@ public class FurthestStateGen {
 			// checks if any of the boxes on tempBoxList are on the goal
 			if (!goalCheck) {
 				// check if the calculated score is above 3000 (value determined through experimentation)
-				if (7 < calculateDifficulty(resultList.get(randState))) {
+				if (6 < calculateDifficulty(resultList.get(randState))) {
 					currScore = true;
 				}
 				// adds the state's index onto possibleStates
